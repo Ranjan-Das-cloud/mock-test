5b97083cb0c0e81b3dc0a8bb3a9020c504d4b492
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-12-03 17:28:49 +0530

    Added submit feature to react app for submission

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Home from './home';
 import Navbar from './navbar';
+import Submit from './result';
 import Test from './test';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Switch>
             <Route path="/" component={Home} exact></Route>
             <Route path="/test/:id" component={Test}></Route>
+            <Route path="/result" component={Submit}></Route>
         </Switch>
       </div>
      );
