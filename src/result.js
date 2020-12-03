f160c6bb81d89ac0a21b854764365c9b1ddd1c52
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-12-03 17:26:42 +0530

    Added card feature to display result obtained

--- src/result.js
+++ src/result.js
@@ -5,7 +5,17 @@ class Submit extends Component {
     render() { 
         return ( 
             <div>
-                
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 offset-3">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h4>You scored 25 marks</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
          );
     }
