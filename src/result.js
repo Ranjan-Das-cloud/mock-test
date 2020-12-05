09305595b0c57132144b41fb710fa4b8f197cedf
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-12-05 14:35:24 +0530

    Updated with desinging features

--- src/result.js
+++ src/result.js
@@ -6,11 +6,11 @@ class Submit extends Component {
         return ( 
             <div>
                 <div className="container">
-                    <div className="row">
+                    <div className="row mt-5">
                         <div className="col-6 offset-3">
                             <div className="card">
                                 <div className="card-body">
-                                    <h4>You scored 25 marks</h4>
+                                    <h4>You scored {this.props.location.state.marks} marks</h4>
                                 </div>
                             </div>
                         </div>
