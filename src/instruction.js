b6b7ca1b277418c5151f6758d2bb3c378ed49e26
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-02 19:47:51 +0530

    Added React component for test instructions

---
+++ src/instruction.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Instructions extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h2>Let's the test begin !!</h2>
+            </div>
+         );
+    }
+}
+ 
+export default Instructions;
