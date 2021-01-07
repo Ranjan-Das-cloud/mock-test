7d40feacce1fd1694d284ac37c8b0179bdda3d80
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-07 23:57:11 +0530

    Initialized pagination system

--- src/home.js
+++ src/home.js
@@ -46,6 +46,23 @@ class Home extends Component {
                         </div>
 
                     </div>
+                    <div className="row mt-5">
+                        <nav aria-label="...">
+                            <ul class="pagination mid-auto">
+                                <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item active" aria-current="page">
+                                <a class="page-link" href="#">2</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
          );
