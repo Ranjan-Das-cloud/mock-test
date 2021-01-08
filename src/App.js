d6fe2ea4baa0dffc99f8dd4c6972fb9fd61da50c
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-08 23:57:00 +0530

    Updated with new Route in react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Home from './home';
+import Homesecond from './home2';
 import Instructions from './instruction';
 import Navbar from './navbar';
 import Submit from './result';
@@ -16,6 +17,7 @@ class App extends Component {
         <Switch>
             <Route path="/" component={Instructions} exact></Route>
             <Route path="/home" component={Home}></Route>
+            <Route path="/home/2" component={Homesecond}></Route>
             <Route path="/test/:id" component={Test}></Route>
             <Route path="/result" component={Submit}></Route>
         </Switch>
