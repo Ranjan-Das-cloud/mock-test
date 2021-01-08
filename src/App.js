db310c0ccd59685a344a7638e5117650bfec85ab
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-09 00:51:55 +0530

    Modified with a new Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Home from './home';
 import Homesecond from './home2';
+import Homethird from './home3';
 import Instructions from './instruction';
 import Navbar from './navbar';
 import Submit from './result';
@@ -18,6 +19,7 @@ class App extends Component {
             <Route path="/" component={Instructions} exact></Route>
             <Route path="/home" component={Home}></Route>
             <Route path="/home_2" component={Homesecond}></Route>
+            <Route path="/home_3" component={Homethird}></Route>
             <Route path="/test/:id" component={Test}></Route>
             <Route path="/result" component={Submit}></Route>
         </Switch>
